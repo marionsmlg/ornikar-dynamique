98facd3c35e3811c2bd48a38a8780b4b9c89e916
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-01-10 12:55:24 +0100

    dynamic

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const language = require("./language_fr.json");
 const fs = require("fs");
 nunjucks.configure({ autoescape: true });
 let outString = nunjucks.render("index.njk", language);
-fs.writeFileSync("index.html", outString);
-console.log("index.html file created");
+fs.writeFile("index.html", outString, (err) => {
+  if (err) {
+    throw new err(`erreur`);
+  }
+  console.log(`index.html file created`);
+});
+
+// fs.writeFileSync("index.html", outString);
+// console.log("index.html file created");
 
 // const nunjucks = require("nunjucks");
 // nunjucks.configure({ autoescape: true });
