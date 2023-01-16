2a3866f74ff581a7db6d7417d93d08ef5c3381e2
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-01-16 21:38:26 +0100

    dynamic

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const nunjucks = require("nunjucks");
+const data = require("./data/index.json");
+const fs = require("fs");
+const dataLogin = require("./data/login.json");
+
+nunjucks.configure({ autoescape: true });
+
+let templateMenu = nunjucks.render("./src/template/index.njk", data);
+
+fs.rm("./dist", { recursive: true, force: true }, (err) => {
+  if (err) {
+    throw err;
+  }
+  fs.mkdir("./dist", function (error) {
+    if (error) {
+      throw error;
+    }
+    fs.writeFile("./dist/index.html", templateMenu, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`index.html file created`);
+    });
+
+    let templateLogin = nunjucks.render("./src/template/login.njk", dataLogin);
+    fs.writeFile("./dist/login.html", templateLogin, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`login.html file created`);
+    });
+
+    fs.copyFile("./src/css/index.css", "./dist/index.css", (err) => {
+      if (err) throw err;
+      console.log("Le fichier index.css a été copié!");
+    });
+    fs.copyFile("./src/css/global.css", "./dist/global.css", (err) => {
+      if (err) throw err;
+      console.log("Le fichier global.css a été copié!");
+    });
+    fs.copyFile("./src/css/login.css", "./dist/login.css", (err) => {
+      if (err) throw err;
+      console.log("Le fichier login.css a été copié!");
+    });
+  });
+});
+
+// fs.writeFileSync("index.html", outString);
+// console.log("index.html file created");
+
+// const nunjucks = require("nunjucks");
+// nunjucks.configure({ autoescape: true });
+// let helloStr = nunjucks.renderString("Hello {{username}}", {
+//   username: "Marion",
+// });
+// console.log(helloStr);
