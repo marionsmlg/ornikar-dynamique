340a5c77eb0b8f47d3e8adfe2548443f213321ad
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-01-30 21:35:31 +0100

    dynamic

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,13 @@
-document.getElementById("burgerButton").addEventListener("click", () => {
-  console.log("coucou");
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("hello");
+  let menuIsOpen = true;
+  document.getElementById("burgerButton").addEventListener("click", () => {
+    if (menuIsOpen) {
+      document.getElementById("smallMenu").style.display = "none";
+      menuIsOpen = false;
+    } else {
+      document.getElementById("smallMenu").style.display = "flex";
+      menuIsOpen = true;
+    }
+  });
 });
