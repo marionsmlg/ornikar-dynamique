3adbb8eaac379a82937d2ea8977a8f2f452d9123
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-02-15 01:17:21 +0100

    articlePages

---
+++ scripts/generateArticles.js
@@ -0,0 +1,41 @@
+import fsp from "fs/promises";
+import { customAlphabet } from "nanoid";
+import { randParagraph, randPhrase, randTextRange } from "@ngneat/falso";
+
+const arrOfArticles = [];
+const arrOfCategories = [
+  "Assurance auto",
+  "Code de la route",
+  "Permis de conduire",
+  "Conduite accompagnée",
+  "Informations",
+  "Mecanique",
+  "Conduite",
+];
+
+function getRandomFromArr(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+const nanoid = customAlphabet("0123456789qwertyuiopasdfghjklzxcvbnm", 10);
+
+for (let i = 0; i <= 97; i++) {
+  const newArticle = {
+    id: nanoid(),
+    title: randPhrase(),
+    description: randParagraph(),
+    category: getRandomFromArr(arrOfCategories),
+    content: randTextRange({ min: 2000, max: 3000 }),
+    img: "https://picsum.photos/300/200",
+  };
+  arrOfArticles.push(newArticle);
+}
+
+const strOfArticles = JSON.stringify(arrOfArticles, null, "\t");
+
+async function generateArticles() {
+  await fsp.writeFile("./scripts/articles.json", strOfArticles);
+}
+
+generateArticles();
--- src/index.js
+++ src/index.js
@@ -1,84 +1,109 @@
-const nunjucks = require("nunjucks");
-const dataIndex = require("./data/index.json");
-const dataLogin = require("./data/login.json");
-const { minify } = require("html-minifier-terser");
-const fsp = require("fs/promises");
-const cleanCSS = require("clean-css");
-const { minify: minifyterser } = require("terser");
-const path = require("path");
+import fsp from "fs/promises";
+import path from "path";
+import nunjucks from "nunjucks";
 nunjucks.configure({ autoescape: true });
+import { minify } from "html-minifier-terser";
+import cleanCSS from "clean-css";
+import { minify as minifyterser } from "terser";
+import slugify from "@sindresorhus/slugify";
 
 const args = process.argv.slice(2);
+const isDev = args[0] === "dev";
 
-async function minifyCssOrJs(src, dest) {
-  if (path.extname(src) === ".css") {
-    const data = await fsp.readFile(src, "utf8");
-    const minifiedData = await new cleanCSS().minify(data);
-    await fsp.writeFile(dest, minifiedData.styles);
-    console.log("The css file has been minified");
-  } else if (path.extname(src) === ".js") {
-    const data = await fsp.readFile(src, "utf8");
-    const minifiedData = await minifyterser(data);
-    await fsp.writeFile(dest, minifiedData.code);
-    console.log("The js file has been minified");
+async function handleHtml(njkPath, jsonPath, dest) {
+  const dataJson = await fsp.readFile(jsonPath);
+  const dataJsonParse = JSON.parse(dataJson);
+  const dataHtml = nunjucks.render(njkPath, dataJsonParse);
+  if (isDev) {
+    await fsp.writeFile(dest, dataHtml);
+    console.log(`${path.basename(dest)} file created`);
   } else {
-    throw new err("The file used is not a css or js format");
+    const minifedDataHtml = await minify(dataHtml, {
+      removeAttributeQuotes: true,
+      collapseWhitespace: true,
+      removeComments: true,
+    });
+    await fsp.writeFile(dest, minifedDataHtml);
+    console.log(`${path.basename(dest)} file created and minified`);
   }
 }
 
-async function copyFileinDist(src, dest) {
-  await fsp.copyFile(src, dest);
-  console.log(`${path.basename(src)} file has been copied`);
+async function handleArticles(njkPath, jsonPath) {
+  const dataJson = await fsp.readFile(jsonPath);
+  const dataJsonParse = JSON.parse(dataJson);
+  const articles = dataJsonParse.articles;
+
+  for (const article of articles) {
+    const dest = `./dist/blog/${slugify(article.title)}${article.id}.html`;
+
+    const data = {
+      titlePage: article.title,
+      openGraphTitle: article.title,
+      openGraphImg: article.img,
+      openGraphDescription: article.description,
+      openGraphUrl: "https://ornikar-dynamique.vercel.app/",
+      cssGlobal: "/global.css",
+      cssFile: "./articles.css",
+      jsFile: "/global.js",
+      article,
+    };
+
+    const html = nunjucks.render(njkPath, data);
+    await fsp.writeFile(dest, html);
+  }
 }
 
-const minifyOptionsHtml = {
-  removeAttributeQuotes: true,
-  collapseWhitespace: true,
-  removeComments: true,
-};
+async function handleCss(src, dest) {
+  if (isDev) {
+    await fsp.copyFile(src, dest);
+    console.log(`${path.basename(src)} file has been copied`);
+  } else {
+    const data = await fsp.readFile(src, "utf8");
+    const minifiedData = await new cleanCSS().minify(data);
+    await fsp.writeFile(dest, minifiedData.styles);
+    console.log(`${path.basename(src)} file has been minified`);
+  }
+}
+
+async function handleJs(src, dest) {
+  if (isDev) {
+    await fsp.copyFile(src, dest);
+    console.log(`${path.basename(src)} file has been copied`);
+  } else {
+    const data = await fsp.readFile(src, "utf8");
+    const minifiedData = await minifyterser(data);
+    await fsp.writeFile(dest, minifiedData.code);
+    console.log(`${path.basename(src)} file has been minified`);
+  }
+}
 
 async function main() {
   await fsp.rm("./dist", { recursive: true, force: true });
   await fsp.mkdir("./dist");
-  const dataIndexHtml = nunjucks.render("./src/template/index.njk", dataIndex);
-  const dataLoginHtml = nunjucks.render("./src/template/login.njk", dataLogin);
-  if (args[0] === "dev") {
-    await Promise.all([
-      fsp.writeFile("./dist/index.html", dataIndexHtml),
-      console.log(`index.html file created`),
-      copyFileinDist("./src/css/index.css", "./dist/index.css"),
+  await Promise.all([fsp.mkdir("./dist/member"), fsp.mkdir("./dist/blog")]);
 
-      copyFileinDist("./src/css/global.css", "./dist/global.css"),
+  await Promise.all([
+    handleHtml(
+      "./src/template/index.njk",
+      "./src/data/index.json",
+      "./dist/index.html"
+    ),
+    handleCss("./src/css/index.css", "./dist/index.css"),
 
-      copyFileinDist("./src/js/global.js", "./dist/global.js"),
+    handleCss("./src/css/global.css", "./dist/global.css"),
 
-      fsp.mkdir("./dist/member"),
-    ]);
+    handleJs("./src/js/global.js", "./dist/global.js"),
 
-    await Promise.all([
-      fsp.writeFile("./dist/member/login.html", dataLoginHtml),
-      console.log(`login.html file created`),
+    handleHtml(
+      "./src/template/login.njk",
+      "./src/data/login.json",
+      "./dist/member/login.html"
+    ),
+    handleCss("./src/css/login.css", "./dist/member/login.css"),
 
-      copyFileinDist("./src/css/login.css", "./dist/member/login.css"),
-    ]);
-  } else {
-    const minifedDataIndexHtml = await minify(dataIndexHtml, minifyOptionsHtml);
-    await fsp.writeFile("./dist/index.html", minifedDataIndexHtml);
-    console.log(`index.html file created and minified`);
-
-    await Promise.all([
-      minifyCssOrJs("./src/css/index.css", "./dist/index.css"),
-      minifyCssOrJs("./src/css/global.css", "./dist/global.css"),
-      minifyCssOrJs("./src/js/global.js", "./dist/global.js"),
-      fsp.mkdir("./dist/member"),
-    ]);
-
-    const minifedDataLoginHtml = await minify(dataLoginHtml, minifyOptionsHtml);
-    await fsp.writeFile("./dist/member/login.html", minifedDataLoginHtml);
-    console.log(`login.html file created and minified`);
-
-    minifyCssOrJs("./src/css/login.css", "./dist/member/login.css");
-  }
+    handleArticles("./src/template/articles.njk", "./src/data/articles.json"),
+    handleCss("./src/css/articles.css", "./dist/blog/articles.css"),
+  ]);
 }
 
 main();
